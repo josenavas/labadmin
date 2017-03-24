68fe555d57c844d1d3a2531f3ff89e40f36ae15c
Jose Navas <josenavasmolina@gmail.com>
2017-03-24 11:36:00 -0700

    flake8

--- knimin/handlers/ag_new_barcode.py
+++ knimin/handlers/ag_new_barcode.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 from tornado.web import authenticated, HTTPError
-from tornado.escape import xhtml_escape
 from knimin.handlers.base import BaseHandler
 from knimin.handlers.access_decorators import set_access
 
